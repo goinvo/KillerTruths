9101c97a9e03c829842ccdaf7e7b5957ff34b58a
Jennifer Patel <jennifer@goinvo.com>
2017-02-17 16:48:49 -0500

    wip: trying to make it responsive

--- js/killer-chart.js
+++ js/killer-chart.js
@@ -1,3 +1,8 @@
+var margin = {top: 20, right: 20, bottom: 50, left: 100},
+    width = parseInt(d3.select("#killer-chart").style("width")) - margin.left - margin.right;
+var barHeight = 40;
+var mobile_threshold = 500;
+
 d3.json("data/killer-data.json", function(error, data) {
     if (error) throw error;
 
@@ -8,14 +13,18 @@ d3.json("data/killer-data.json", function(error, data) {
     });
     console.log(data[0].name)
 
-    var width = parseInt(d3.select('#killer-chart').style('width'), 10);
-    var barHeight = 40;
     var height = barHeight * data.length;
+
+    var formatNum = d3.format("0,000");
 
     data.forEach(function(d, i) { 
       max = d3.max(d.value);
       min = d3.min(d.value);
     });
+
+    console.log(d3.max(d.value));
+
+    var barWidth = width * 6;
 
     var x = d3.scale.linear()
         .domain([0, max/1000])
@@ -23,7 +32,7 @@ d3.json("data/killer-data.json", function(error, data) {
     //var y = d3.scale.linear().range([height, 0]);
 
     var chart = d3.select('#killer-chart').append("svg")
-        .attr('width', width)
+        .attr("width", width + margin.left + margin.right)
         .attr('height', height);
 
     var bar = chart.selectAll('g')
@@ -34,12 +43,12 @@ d3.json("data/killer-data.json", function(error, data) {
             return 'translate(0,' + barHeight * i + ')';
         });
 
-    bar.append('rect')
+    /*bar.append('rect')
         .attr('class', 'bar')
         .attr('width', function(d){
-            return d.value / 5500;
+            return d.value / 5100;
         })
-        .attr('height', barHeight -1);
+        .attr('height', barHeight);
 
     bar.append('text')
         .attr('class', 'value')
@@ -57,17 +66,51 @@ d3.json("data/killer-data.json", function(error, data) {
         .attr('dy', '.35em')
         .text(function(d){
             return d.name;
+        });*/
+
+    bar.append('rect')
+        .attr('class', 'bar')
+        .attr('x', 290)
+        .attr('y', barHeight / 2.5)
+        .attr('width', function(d){
+            return d.value / barWidth;
+        })
+        .attr('height', barHeight);
+
+    bar.append('text')
+        .attr('class', 'value')
+        .attr('x', 215)
+        .attr('y', barHeight / 2)
+        .attr('dy', '.35em')
+        .text(function(d){
+            return formatNum(d.value);
         });
 
-    d3.select(window).on('resize', resize); 
+    bar.append('text')
+        .attr('class', 'name')
+        .attr('x', 200)
+        .attr('y', barHeight / 2)
+        .attr('dy', '.35em')
+        .text(function(d){
+            return d.name;
+        });
 
     function resize() {
-        console.log("resizing");
-        // update width
-        width = parseInt(d3.select('#killer-chart').style('width'), 10);
+      console.log("resizing");
+      var width = parseInt(d3.select("#killer-chart").style("width")) - margin.left - margin.right;
 
-        // resize the chart
-        d3.select(chart.node().parentNode)
-            .style('width', (width) + 'px');
-    }
+      // Update the range of the scale with new width/height
+      x.range([0, width]);
+      console.log(width);
+      // Force D3 to recalculate and update the line
+        bar.attr("width", function(d){
+            return d.value / barWidth;
+        });
+    };
+
+    // Call the resize function whenever a resize event occurs
+    d3.select(window).on('resize', resize);
+
+    // Call the resize function
+    resize();
 });
