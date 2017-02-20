4112a2d893654828b9c36767306ebe3f34f2cffb
Jennifer Patel <jennifer@goinvo.com>
2017-02-20 12:09:30 -0500

    mobile chart applied

--- js/killer-chart.js
+++ js/killer-chart.js
@@ -1,9 +1,9 @@
 var margin = {top: 20, right: 20, bottom: 50, left: 300},
-    width = parseInt(d3.select("#killer-chart").style("width")) - margin.left - margin.right;
+    width = parseInt(d3.select('#killer-chart').style('width')) - margin.left - margin.right;
 var barHeight = 40;
-var mobile_threshold = 500;
+var mobileThreshold = 550;
 
-d3.json("data/killer-data.json", function(error, data) {
+d3.json('data/killer-data.json', function(error, data) {
     if (error) throw error;
 
     data.forEach(function(d) {
@@ -15,7 +15,7 @@ d3.json("data/killer-data.json", function(error, data) {
 
     var height = barHeight * data.length;
 
-    var formatNum = d3.format("0,000");
+    var formatNum = d3.format('0,000');
 
     data.forEach(function(d, i) { 
       max = d3.max(data, function(d) {return d.value;});
@@ -43,78 +43,96 @@ d3.json("data/killer-data.json", function(error, data) {
             return 'translate(0,' + barHeight * i + ')';
         });
 
-        //styles for desktop - scale this
-    bar.append('rect')
-        .attr('class', 'bar')
-        .attr('x', 290)
-        .attr('y', barHeight / 2.5)
-        .attr('width', function(d){
-            return x(d.value);
-        })
-        .attr('height', barHeight);
+    function desktopSize() {
+            //styles for desktop - scale this
+        bar.append('rect')
+            .attr('class', 'bar')
+            .attr('x', 290)
+            .attr('y', barHeight / 2.5)
+            .attr('width', function(d){
+                return x(d.value);
+            })
+            .attr('height', barHeight);
 
-    bar.append('text')
-        .attr('class', 'value')
-        .attr('x', 215)
-        .attr('y', barHeight / 2)
-        .attr('dy', '.35em')
-        .text(function(d){
-            return formatNum(d.value);
-        });
+        bar.append('text')
+            .attr('class', 'value')
+            .attr('x', 215)
+            .attr('y', barHeight / 2)
+            .attr('dy', '.35em')
+            .text(function(d){
+                return formatNum(d.value);
+            });
 
-    bar.append('text')
-        .attr('class', 'name')
-        .attr('x', 200)
-        .attr('y', barHeight / 2)
-        .attr('dy', '.35em')
-        .text(function(d){
-            return d.name;
-        });
-
-    // Define responsive behavior
-    function resize() {
-      var width = parseInt(d3.select("#killer-chart").style("width")) - margin.left - margin.right;
-
-      // Update the range of the scale with new width/height
-      x.range([0, width]);
-
-      // Force D3 to recalculate and update the line
-      bar.selectAll('rect')
-        .attr("width", function(d) { return x(d.value); });
+        bar.append('text')
+            .attr('class', 'name')
+            .attr('x', 200)
+            .attr('y', barHeight / 2)
+            .attr('dy', '.35em')
+            .text(function(d){
+                return d.name;
+            });
     };
 
-    // Call the resize function whenever a resize event occurs
-    d3.select(window).on('resize', resize);
+    function mobileSize() {
+        //blow away initial chart if one exists
+        d3.select('#killer-chart').empty();
 
-    // Call the resize function
-    resize();
+        //reset width and xScale
+        var margin = {top: 0, right: 0, bottom: 0, left: 0}
+            width = parseInt(d3.select('#killer-chart').style('width')) - margin.left - margin.right;
+
+        var x = d3.scale.linear()
+            .domain([0, max])
+            .range([0, width]);
+
+        /*apply class for mobile sizes, so there 
+            are no awkward transitions when resizing*/
+        chart.attr('class', 'mobile');
+
+        //redraw chart for mobile
+        bar.append('rect')
+            .attr('class', 'bar')
+            .attr('width', function(d){
+                return x(d.value);
+            })
+            .attr('height', barHeight);
+
+        bar.append('text')
+            .attr('class', 'value')
+            .attr('x', 80)
+            .attr('y', barHeight / 2)
+            .attr('dy', '.35em')
+            .text(function(d){
+                return d.value;
+            });
+
+        bar.append('text')
+            .attr('class', 'name')
+            .attr('x', 100)
+            .attr('y', barHeight / 2)
+            .attr('dy', '.35em')
+            .text(function(d){
+                return d.name;
+            });
+    };
+
+        // Define responsive behavior
+    function resize() {
+        var width = parseInt(d3.select('#killer-chart').style('width')) - margin.left - margin.right;
+      
+        x.range([0, width]);
+
+        bar.selectAll('rect')
+            .attr('width', function(d) { return x(d.value); });
+    };
+
+    // draw a desktop chart unless the window size is below 550px
+    if (window.innerWidth < mobileThreshold) {
+        mobileSize();
+    } else {
+        desktopSize();
+        d3.select(window).on('resize', resize);
+        resize();
+    }
 
 });
-
-function mobilesize() {
-    //styles for mobile, <500 width - scale
-    /*bar.append('rect')
-        .attr('class', 'bar')
-        .attr('width', function(d){
-            return d.value / 5100;
-        })
-        .attr('height', barHeight);
-
-    bar.append('text')
-        .attr('class', 'value')
-        .attr('x', 80)
-        .attr('y', barHeight / 2)
-        .attr('dy', '.35em')
-        .text(function(d){
-            return d.value;
-        });
-
-    bar.append('text')
-        .attr('class', 'name')
-        .attr('x', 100)
-        .attr('y', barHeight / 2)
-        .attr('dy', '.35em')
-        .text(function(d){
-            return d.name;
-        });*/
-}
